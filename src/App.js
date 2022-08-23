907ace1d6def27d0ddea7063862309bb6993970d
priyanshu1044 <priyanshupatel1044@gmail.com>
2022-08-23 21:05:05 +0530

    create app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>let's build twitter clone</h1>
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
